0c57d77fa0d6370f0342a80d7323ace2c743fdc9
Joseph Padfield <joseph.padfield@gmail.com>
2024-10-08 16:28:06 +0100

    media query

--- index.js
+++ index.js
@@ -4,7 +4,39 @@ const aboutBox = document.querySelector('.about-container')
 const closeButton = document.querySelector('.close-button')
 
 const projectHtml = `
- <div class="project">
+        <div class="project">
+            <p class="project-title">Reels From the Crypt</p>
+            <img src="./img/reels-from-the-crypt.png" class="project-img">
+            <span>
+            <a class="nav-links">Live link</a><br>
+            <a class="nav-links">Github repo</a>
+            </span>
+        </div>
+        <div class="project">
+            <p class="project-title">Tom Waits Ipsum</p>
+            <img src="./img/tom-waits-ipsum.png" class="project-img">
+            <span>
+            <a class="nav-links">Live link</a><br>
+            <a class="nav-links">Github repo</a>
+            </span>
+        </div>
+        <div class="project">
+            <p class="project-title">Famous Last Words</p>
+            <img src="./img/movie_quote_quiz.png" class="project-img">
+            <span>
+            <a class="nav-links">Live link</a><br>
+            <a class="nav-links">Github repo</a>
+            </span>
+        </div>
+        <div class="project">
+            <p class="project-title">Furniture Store</p>
+            <img src="./img/furniture-store.png" class="project-img">
+            <span>
+            <a class="nav-links">Live link</a><br>
+            <a class="nav-links">Github repo</a>
+            </span>
+        </div>
+                <div class="project">
             <p class="project-title">Reels From the Crypt</p>
             <img src="./img/reels-from-the-crypt.png" class="project-img">
             <span>
@@ -39,6 +71,7 @@ const projectHtml = `
 `
 
 const projectContent = document.createElement('div')
+projectContent.classList.add('project-content')
 document.querySelector('.projects').addEventListener('click', () => {
 
     projectBox.innerHTML = ''
@@ -53,9 +86,9 @@ document.querySelector('.projects').addEventListener('click', () => {
     })
 })
 
-
 const aboutText = document.createElement('p')
-const about =  `<p>
+aboutText.classList.add('about-text')
+const about = `
     <span class="box-header">About</span><br><br>
     I am a passionate and enthusiastic full-stack developer, recently graduated from an intensive training
     program at iO Academy in Bath. After a rewarding 12-year career as an opera singer, and driven by a lifelong
@@ -65,12 +98,11 @@ const about =  `<p>
     Combining my skill-set from a long career in the performing arts with the skills and technologies that I
     have learnt during my time at iO Academy has given me a unique approach to problem-solving and team-work
     within this field, and I am excited to continue my learning and development as my career progresses.
-</p>`
+`
 
-    aboutText.innerHTML = about
+aboutText.innerHTML = about
 
 document.querySelector('.about').addEventListener('click', () => {
-    // aboutBox.classList.toggle('animate-about-box')
 
     projectBox.innerHTML = ''
     projectBox.appendChild(closeButton)
@@ -86,6 +118,3 @@ document.querySelector('.close-button').addEventListener('click', () => {
         img.classList.toggle('animate-img')
     })
 })
-
-
-
