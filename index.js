bcdbda37d1e50d0f922c886be7571d6cef09e127
Joseph Padfield <joseph.padfield@gmail.com>
2024-10-09 08:24:36 +0100

    project links and images. a few more details to go

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const projectBox = document.querySelector('.project-container')
 const projectImg = document.querySelectorAll('.project-img')
-const aboutBox = document.querySelector('.about-container')
 const closeButton = document.querySelector('.close-button')
 
 const projectHtml = `
@@ -8,64 +7,64 @@ const projectHtml = `
             <p class="project-title">Reels From the Crypt</p>
             <img src="./img/reels-from-the-crypt.png" class="project-img">
             <span>
-            <a class="nav-links">Live link</a><br>
-            <a class="nav-links">Github repo</a>
+            <a target="_blank" href="https://2024-josephp.dev.io-academy.uk/collection_app/" class="nav-links">Live link</a><br>
+            <a target="_blank" href="https://github.com/joseph-padfield/collection-app-project" class="nav-links">Github repo</a>
             </span>
         </div>
         <div class="project">
             <p class="project-title">Tom Waits Ipsum</p>
             <img src="./img/tom-waits-ipsum.png" class="project-img">
             <span>
-            <a class="nav-links">Live link</a><br>
-            <a class="nav-links">Github repo</a>
+            <a target="_blank" href="https://tom-waits-ipsum-generator.2024-josephp.dev.io-academy.uk/" class="nav-links">Live link</a><br>
+            <a target="_blank" href="https://github.com/joseph-padfield/tom-waits-ipsum-generator" class="nav-links">Github repo</a>
             </span>
         </div>
         <div class="project">
             <p class="project-title">Famous Last Words</p>
             <img src="./img/movie_quote_quiz.png" class="project-img">
             <span>
-            <a class="nav-links">Live link</a><br>
-            <a class="nav-links">Github repo</a>
+            <a target="_blank" href="https://joseph-padfield.github.io/movie-quote-quiz-project/" class="nav-links">Live link</a><br>
+            <a target="_blank" href="https://github.com/joseph-padfield/movie-quote-quiz-project" class="nav-links">Github repo</a>
             </span>
         </div>
         <div class="project">
-            <p class="project-title">Furniture Store</p>
+            <p class="project-title">Furniture Store Group Project</p>
             <img src="./img/furniture-store.png" class="project-img">
             <span>
-            <a class="nav-links">Live link</a><br>
-            <a class="nav-links">Github repo</a>
+            <a target="_blank" href="https://2024-henryt.dev.io-academy.uk/build/" class="nav-links">Live link</a><br>
+            <a target="_blank" href="https://github.com/iO-Academy/24-jul-hippos-furniture-store-API" class="nav-links">Github repo</a>
             </span>
         </div>
                 <div class="project">
-            <p class="project-title">Reels From the Crypt</p>
-            <img src="./img/reels-from-the-crypt.png" class="project-img">
+            <p class="project-title">How Old are you on Mars?</p>
+            <img src="./img/planet-age-calculator.png" class="project-img">
             <span>
-            <a class="nav-links">Live link</a><br>
-            <a class="nav-links">Github repo</a>
+            <a target="_blank" href="https://joseph-padfield.github.io/how-old-are-you-on-mars/" class="nav-links">Live link</a><br>
+            <a target="_blank" href="https://github.com/joseph-padfield/how-old-are-you-on-mars" class="nav-links">Github repo</a>
             </span>
         </div>
         <div class="project">
-            <p class="project-title">Tom Waits Ipsum</p>
-            <img src="./img/tom-waits-ipsum.png" class="project-img">
+            <p class="project-title">Academy Portal Group Project</p>
+            <img src="./img/AcademyPortal.png" class="project-img">
             <span>
-            <a class="nav-links">Live link</a><br>
-            <a class="nav-links">Github repo</a>
+            <a target="_blank" class="nav-links">Live link</a><br>
+            <a target="_blank" class="nav-links">Github repo</a>
             </span>
         </div>
         <div class="project">
-            <p class="project-title">Famous Last Words</p>
-            <img src="./img/movie_quote_quiz.png" class="project-img">
+            <p class="project-title">Jobsite Group Project</p>
+            <img src="./img/jobsiteScreenShot.png" class="project-img">
             <span>
-            <a class="nav-links">Live link</a><br>
-            <a class="nav-links">Github repo</a>
+            <a target="_blank" href="https://2024-thomask.dev.io-academy.uk/dist/" class="nav-links">Live link</a><br>
+            <a target="_blank" href="https://github.com/iO-Academy/2024-jul-fst-job-search-fe" class="nav-links">Github repo</a>
             </span>
         </div>
         <div class="project">
-            <p class="project-title">Furniture Store</p>
+            <p class="project-title">To Do App</p>
             <img src="./img/furniture-store.png" class="project-img">
             <span>
-            <a class="nav-links">Live link</a><br>
-            <a class="nav-links">Github repo</a>
+            <a target="_blank" class="nav-links">Live link</a><br>
+            <a target="_blank" class="nav-links">Github repo</a>
             </span>
         </div>
 `
@@ -76,20 +75,17 @@ document.querySelector('.projects').addEventListener('click', () => {
 
     projectBox.innerHTML = ''
     projectContent.innerHTML = projectHtml
+
     projectBox.appendChild(closeButton)
     projectBox.appendChild(projectContent)
 
-    projectBox.classList.toggle('animate-box')
+    projectBox.classList.toggle('close')
 
-    projectImg.forEach(img => {  // Iterate over the NodeList
-        img.classList.toggle('animate-img')
-    })
 })
 
 const aboutText = document.createElement('p')
 aboutText.classList.add('about-text')
-const about = `
-    <span class="box-header">About</span><br><br>
+aboutText.innerHTML = `<span class="box-header">About</span><br><br>
     I am a passionate and enthusiastic full-stack developer, recently graduated from an intensive training
     program at iO Academy in Bath. After a rewarding 12-year career as an opera singer, and driven by a lifelong
     love of technology, I decided to embark on a new career path in software development.<br><br>
@@ -97,25 +93,25 @@ const about = `
     within a scrum team using a broad range of technologies.<br><br>
     Combining my skill-set from a long career in the performing arts with the skills and technologies that I
     have learnt during my time at iO Academy has given me a unique approach to problem-solving and team-work
-    within this field, and I am excited to continue my learning and development as my career progresses.
-`
-
-aboutText.innerHTML = about
+    within this field, and I am excited to continue my learning and development as my career progresses.`
 
 document.querySelector('.about').addEventListener('click', () => {
 
     projectBox.innerHTML = ''
+
     projectBox.appendChild(closeButton)
     projectBox.appendChild(aboutText)
 
-    projectBox.classList.toggle('animate-box')
+    projectBox.classList.toggle('close')
+
 })
 
 document.querySelector('.close-button').addEventListener('click', () => {
-    projectBox.classList.toggle('animate-box')
-    projectBox.innerHTML = ''
 
-    projectImg.forEach(img => {  // Iterate over the NodeList
-        img.classList.toggle('animate-img')
-    })
+    projectBox.classList.toggle('close')
+
+    setTimeout(() => {
+        projectBox.innerHTML = ''
+    }, 1000)
+
 })
